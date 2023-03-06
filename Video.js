c5450576e7928ab0468b3c8cf1bde46be83f3538
Sowmiya MV <93089377+Sowmiya0319@users.noreply.github.com>
2023-03-06 12:26:06 +0530

    Add files via upload

---
+++ Video.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {useState,useEffect} from 'react';
+import video from './smoke.mp4';
+
+function Video(){
+    const [a,setA]=useState(0);
+    useEffect(()=>{
+        setTimeout(()=>{
+            if(a===10){
+                alert(" Movie Started");
+                setA((a)=>a+1);
+            }
+            else if(a===30)
+            {
+                alert("Interval");
+                setA((a)=>a+1);
+            }
+            else if(a===100)
+            {
+                alert("Movie Completed");
+                
+            }
+            else{
+            setA((a)=>a+1);
+            }
+            
+
+        },100);
+
+    })
+
+    
+    return(
+        <div>
+  
+        
+        </div>
+    )
+
+}
+export default Video;
